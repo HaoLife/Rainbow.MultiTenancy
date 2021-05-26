c1c009ad028129b08fcebd2f5aa90d4eede05440
w4384 <w4384@DESKTOP-ATHUNVA>
2021-05-26 18:57:39 +0800

    添加远程连接的spa模式，测试

---
+++ samples/Rainbow.MultiTenancy.IdentityServerRemoteSPA/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Rainbow.MultiTenancy.AspNetCore.ApiAuthorization.IdentityServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rainbow.MultiTenancy.IdentityServerRemoteSPA.Data
+{
+    public class ApplicationDbContext : TenantApiAuthorizationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            EntityFrameworkCore.TenantModelBuilderExtensions.AddTenant(builder);
+        }
+    }
+}
---
+++ samples/Rainbow.MultiTenancy.IdentityServerRemoteSPA/Startup.cs
@@ -0,0 +1,119 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Rainbow.MultiTenancy.Extensions.Identity.Stores;
+using Rainbow.MultiTenancy.IdentityServerRemoteSPA.Data;
+
+namespace Rainbow.MultiTenancy.IdentityServerRemoteSPA
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection"))
+                .UseMultiTenancy()
+                );
+
+            services.AddMulitTenancy(options =>
+            {
+                options
+                    .AddDomainTenantResolveContributor("{tenant}.test.com")
+                    .AddHttpTenantResolveContributor()
+                    //.AddDefaultTenantConfiguration(Configuration.GetSection("Tenant"))
+                    ;
+            }).AddTenantEntityFrameworkStores<ApplicationDbContext>();
+
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+            services.AddDefaultIdentity<TenantUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<TenantRole>()
+                .AddTenantIdentityCore()
+                .AddTenantEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddTenantIdentityServer()
+                .AddApiAuthorization<TenantUser, ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.AddMultiTenancy();
+
+            app.UseIdentityServer();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
